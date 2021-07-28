31e188fdb37fc2c9fd741133b7bbb79a2cfb0540
ProklUng <funtik21975@gmail.com>
2021-07-28 12:13:34 +0300

    Some tools

---
+++ Services/Email/EventBridge/EventBridge.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Prokl\BitrixOrdinaryToolsBundle\Services\Email\EventBridge;
+
+use Bitrix\Main\ArgumentException;
+use Bitrix\Main\Mail\EventMessageCompiler;
+use Bitrix\Main\Mail\Internal\EventMessageAttachmentTable;
+use Bitrix\Main\Mail\Internal\EventMessageTable;
+use Bitrix\Main\ObjectPropertyException;
+use Bitrix\Main\SystemException;
+
+/**
+ * Class EventBridge
+ * @package Prokl\BitrixOrdinaryToolsBundle\Services\Email\EventBridge
+ *
+ * @since 28.07.2021
+ */
+class EventBridge
+{
+    /**
+     * Получить скомпилированный текст письма для события.
+     *
+     * @param EventInfo $eventInfo DTO события.
+     * @param array     $context   Значения полей ($arFields).
+     * @param array     $sites     ID сайтов.
+     *
+     * @return array
+     */
+    public function compileMessage(EventInfo $eventInfo, array $context, array $sites = []) : array
+    {
+        $message = EventMessageCompiler::createInstance([
+            'EVENT' => $eventInfo->getEventCode(),
+            'FIELDS' => $context,
+            'MESSAGE' => $eventInfo->getMessageData(),
+            'SITE' => $sites,
+            'CHARSET' => 'UTF8',
+        ]);
+        $message->compile();
+
+        return ['body' => $message->getMailBody(),
+            'mail_to' => $message->getMailTo(),
+            'charset' => $message->getMailCharset(),
+            'subject' => $message->getMailSubject(),
+        ];
+    }
+
+    /**
+     * @param string $codeEvent Код события.
+     * @param array  $sites     ID сайтов.
+     *
+     * @return EventInfo[]
+     *
+     * @throws ArgumentException | ObjectPropertyException | SystemException Битриксовые ошибки.
+     */
+    public function getMessageTemplate(string $codeEvent, array $sites = []) : array
+    {
+        $arSites = ['s1'];
+        if (count($sites) > 0) {
+            $arSites = array_merge($arSites, $sites);
+        }
+
+        $messageDb = EventMessageTable::getList([
+            'select' => ['ID'],
+            'filter' => [
+                '=EVENT_NAME' => $codeEvent,
+                '=EVENT_MESSAGE_SITE.SITE_ID' => $arSites,
+            ],
+            'group' => ['ID']
+        ]);
+
+        $result = [];
+        foreach ($messageDb as $arMessage) {
+            $eventMessage = EventMessageTable::getRowById($arMessage['ID']);
+
+            $eventMessage['FILES'] = [];
+            $attachmentDb = EventMessageAttachmentTable::getList([
+                'select' => ['FILE_ID'],
+                'filter' => ['=EVENT_MESSAGE_ID' => $arMessage['ID']],
+            ]);
+
+            while ($arAttachmentDb = $attachmentDb->fetch()) {
+                $eventMessage['FILE'][] = $arAttachmentDb['FILE_ID'];
+            }
+
+            $result[] = EventInfo::fromArray($eventMessage);
+        }
+
+        return $result;
+    }
+}
